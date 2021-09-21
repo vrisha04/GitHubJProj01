34c3788ddda5541bebf427c84cfe395e8a1c7369
vinay <vinay@DESKTOP-TODPIIC>
2021-09-21 17:48:50 -0500

    first commit

---
+++ src/test/java/package001/TEstsce1.java
@@ -0,0 +1,11 @@
+package package001;
+
+public class TEstsce1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to GitHub Demo...");
+		System.out.println("This is GitHub.");
+	}
+
+}
